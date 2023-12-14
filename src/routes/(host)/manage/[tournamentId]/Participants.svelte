b5995d99f41454dd6a2d0033b6a65ea7f85aa2a2
Joey Rapista <yeojrapista@gmail.com>
2023-12-14 16:20:49 +0800

    added player and team form

--- src/routes/(host)/manage/[tournamentId]/Participants.svelte
+++ src/routes/(host)/manage/[tournamentId]/Participants.svelte
@@ -1,22 +1,119 @@
 <script>
     export let tournament
-    import {  Modal, Label, Input, Checkbox, Select, Button, Helper, Listgroup, ListgroupItem, Avatar, ButtonGroup, InputAddon, Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, Textarea } from 'flowbite-svelte'
+    import { DotsHorizontalOutline, DotsVerticalOutline, GearSolid } from 'flowbite-svelte-icons'
+
+    import {  Modal, Label, Input, Checkbox, Select, Button, Hr, Helper, Listgroup, ListgroupItem, Avatar, ButtonGroup, InputAddon, Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell, Textarea } from 'flowbite-svelte'
     const { participant_type } = tournament
-    
+    let playerModal = false 
+    let selectedTeam = {}
     let participants = tournament?.expand?.['participants(tournament)'] ?? []
     
-</script>
-<form class="space-y-2" action="?/createParticipant" method="POST">
-    <Label>Name</Label>
-    <Input name='name' required> </Input>
-    <Button type="submit">Add participant</Button>
-</form>
+    const handleTeamClick = (team) => { 
+        selectedTeam = team
+        playerModal = true
+    }
 
-<div class="text-white">
-    <ul>
+    const handleAddPlayer = (event) => { 
+        const newPlayer = Object.fromEntries(new FormData(event.target) )
+        console.log(newPlayer)
+        selectedTeam.players = [...selectedTeam.players ?? [], newPlayer]
+        // teamPlayersList = selectedTeam.players
+        // console.log(teamPlayersList)
+        // console.log(selectedTeam)
+        // console.log(participants)
+
+    }
+</script>
+
+{#if participant_type === 'team'}    
+<form class="space-y-2 mb-4" action="?/createParticipant" method="POST">
+    <Label>Team details</Label>
+    <ButtonGroup class="w-full"> 
+        <Input  type="text" name="name" placeholder="Team name" required />
+        <Input  type="text" name="social" placeholder="https://www.facebook.com/joey_rapista" />
+        <Button outline color="primary" type="submit">Add</Button> 
+      </ButtonGroup> 
+</form>
+<Hr/>
+<Table>
+    <TableHead> 
+            <TableHeadCell>Name</TableHeadCell>
+            <TableHeadCell>Link</TableHeadCell>
+            <TableHeadCell>Players</TableHeadCell> 
+    </TableHead>
+    <TableBody>
         {#each participants as participant} 
-        <li>{participant.name}</li>
-        {/each}
-    </ul>
-</div>
+            <TableBodyRow> 
+                <TableBodyCell>{participant.name}</TableBodyCell>
+                <TableBodyCell>{participant.social ?? ''}</TableBodyCell>
+                <TableBodyCell><Button on:click={()=> handleTeamClick(participant)}>{participant.players?.players?.length ?? '0'} Players</Button></TableBodyCell>
+            </TableBodyRow>
+        {/each} 
+    </TableBody>
+</Table>
  
+  
+  <Modal bind:open={playerModal} size="lg" autoclose={false} class="w-full">
+    <form class="space-y-2 mb-4" on:submit|preventDefault={handleAddPlayer}>
+        <Label>Player details</Label>
+        <ButtonGroup class="w-full"> 
+            <Input  type="text" name="name" placeholder="Player name" required />
+            <Input  type="text" name="social" placeholder="https://www.facebook.com/joey_rapista" required />
+            <Input  type="text" name="identifier" placeholder="IGN, jersey number etc..." required />
+            <Input  type="text" name="position" placeholder="Mid, Point Guard etc..." required />
+            <Button outline color="primary" type="submit">Add</Button> 
+          </ButtonGroup> 
+    </form>
+    <Hr/>
+    <Table>
+        <TableHead> 
+                <TableHeadCell>Name</TableHeadCell>
+                <TableHeadCell>Link</TableHeadCell>
+                <TableHeadCell>Identifier</TableHeadCell> 
+                <TableHeadCell>Position</TableHeadCell> 
+        </TableHead>
+        <TableBody>
+            {#each selectedTeam.players ?? [] as player} 
+                <TableBodyRow> 
+                    <TableBodyCell>{player.name ?? ''}</TableBodyCell>
+                    <TableBodyCell>{player.social ?? ''}</TableBodyCell>
+                    <TableBodyCell>{player.identifier ?? ''}</TableBodyCell>
+                    <TableBodyCell>{player.position ?? ''}</TableBodyCell>
+                </TableBodyRow>
+            {/each} 
+        </TableBody>
+    </Table>
+  </Modal>
+{/if}
+
+
+{#if participant_type === 'player'}
+<form class="space-y-2 mb-4" action="?/createParticipant" method="POST">
+    <Label>Player details</Label>
+    <ButtonGroup class="w-full"> 
+        <Input  type="text" name="name" placeholder="player name" required />
+        <Input  type="text" name="identifier" placeholder="game username, jersey number etc... " required />
+        <Input  type="text" name="social" placeholder="https://www.facebook.com/joey_rapista" />
+        <Button color="primary" type="submit">Add</Button> 
+      </ButtonGroup> 
+</form>
+<Hr/>
+<Table>
+    <TableHead> 
+            <TableHeadCell>Name</TableHeadCell>
+            <TableHeadCell>Identifier</TableHeadCell>
+            <TableHeadCell>Link</TableHeadCell> 
+            <TableHeadCell></TableHeadCell>
+    </TableHead>
+    <TableBody>
+        {#each participants as participant} 
+            <TableBodyRow> 
+                <TableBodyCell>{participant.name ?? ''}</TableBodyCell>
+                <TableBodyCell>{participant.identifier ?? ''}</TableBodyCell>
+                <TableBodyCell>{participant.social ?? ''}</TableBodyCell>
+                <TableBodyCell><DotsVerticalOutline/></TableBodyCell>
+            </TableBodyRow>
+        {/each} 
+    </TableBody>
+</Table>
+{/if}
--- src/routes/(host)/manage/stage/+server.js
+++
@@ -1,8 +0,0 @@
-import { json, redirect } from '@sveltejs/kit';
-
-export const POST = ({ request }) => {
-    console.log(request)
-    return json({
-        name: 'yey'
-    })
-};
